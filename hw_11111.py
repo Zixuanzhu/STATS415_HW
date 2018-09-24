3fe00fa4c2082d932b0c78a9a382980fce9a77f7
Zixuan Zhu <zxzhunku@gmail.com>
2018-09-23 23:43:53 -0400

    Changes deleted

--- hw_11111.py
+++ hw_11111.py
@@ -7,21 +7,7 @@ a = 3
 b = 4
 c = a + b
 c += 1
+x = 1
+y = 1
 d = c
 
-
-data_p = pd.read_table('locLinRegData.txt')
-n = np.shape(data)[0]
-feature = data[:,1].reshape(n,1)
-label = data[:,0].reshape(n,1)
-ones = np.ones(shape=(len(feature),1))
-X = np.append(ones, feature, axis=1)
-y = label
-beta = np.linalg.inv(X.T.dot(X)).dot(X.T).dot(y)
-
-a = np.linspace(-5,13,100)
-b = beta[0] + beta[1]*a
-
-plt.scatter(feature,label)
-plt.plot(a, b)
-plt.show()
